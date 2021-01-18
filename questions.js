9f4d6fb06195bef0a881359941e4cc2150a5fbb5
Lumarite <77590345+Lumarite@users.noreply.github.com>
2021-01-17 20:15:48 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -11,8 +11,8 @@ questions = [
     {
         "question": "It is necessary for the government to intervene in the economy to protect consumers.",
         "effect": {
-            "econ": 10,
-            "dipl": 0,
+            "econ": 0,
+            "dipl": -10,
             "govt": 0,
             "scty": 0
         }
@@ -20,19 +20,19 @@ questions = [
     {
         "question": "The freer the markets, the freer the people.",
         "effect": {
-            "econ": -10,
+            "econ": 0,
             "dipl": 0,
-            "govt": 0,
+            "govt": 10,
             "scty": 0
         }
     },
     {
         "question": "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
         "effect": {
-            "econ": -10,
+            "econ": 0,
             "dipl": 0,
             "govt": 0,
-            "scty": 0
-        }
+            "scty": 10
+        },
 
 ];
