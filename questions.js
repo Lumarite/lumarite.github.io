4193d30a93f0303ecf3165528e62f54a72ae08bb
Lumarite <77590345+Lumarite@users.noreply.github.com>
2021-01-17 18:56:37 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+questions = [
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The freer the markets, the freer the people.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+
+];
